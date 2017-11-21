02cf9865b9ccbee7a2f91f7657f5a1a65753fe63
Horacio Santos Chi <529023962@qq.com>
2017-11-22 01:28:26 +0800

    修改了下我们的客户页

--- app/scripts/components/customer.js
+++ app/scripts/components/customer.js
@@ -6,26 +6,11 @@ app.component(
         controllerAs: 'customer',
         controller: function ($state) {
             var self = this;
-            self.logos = [
-                "../images/logo1.png",
-                "../images/logo2.png",
-                "../images/logo3.png",
-                "../images/logo4.png",
-                "../images/logo5.png",
-                "../images/logo6.png",
-                "../images/logo7.png",
-                "../images/logo8.png",
-                "../images/logo9.png",
-                "../images/logo10.png",
-                "../images/logo11.png",
-                "../images/logo12.png",
-                "../images/logo13.png",
-                "../images/logo14.png",
-                "../images/logo15.png",
-                "../images/logo16.png"
-
-            ];
+            self.logos = [];
             self.$onInit = function() {
+                for(var i=1;i<17;i++){
+                    self.logos.push("\../images/logo"+i+".png");
+                }
             };
 
         }
