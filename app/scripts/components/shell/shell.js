4ec30d07affc414b53acd5525fa3fd2342897aaa
Horacio Santos Chi <529023962@qq.com>
2017-11-21 17:36:29 +0800

    将footer固定再底部

--- app/scripts/components/shell/shell.js
+++ app/scripts/components/shell/shell.js
@@ -3,8 +3,20 @@
 app.controller('shellController', function ($state) {
       var self = this;
       self.$onInit = function() {
-
+          footerPosition();
       };
+    function footerPosition(){
+        $("footer").removeClass("fixed-bottom");
+        var contentHeight = document.body.scrollHeight,//网页正文全文高度
+            winHeight = window.innerHeight;//可视窗口高度，不包括浏览器顶部工具栏
+        if(!(contentHeight > winHeight)){
+            //当网页正文高度小于可视窗口高度时，为footer添加类fixed-bottom
+            $("footer").addClass("fixed-bottom");
+        } else {
+            $("footer").removeClass("fixed-bottom");
+        }
+    }
 
+    $(window).resize(footerPosition);
   }
 );
